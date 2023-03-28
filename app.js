4e28e6d6ca79e49d45928a05aeabcd741781d176
Devin <devink12110@gmail.com>
2023-03-28 12:00:07 -0400

    Enable single digit operations

--- app.js
+++ app.js
@@ -1,52 +1,74 @@
+// to get multiple digit numbers: 
+// num1 is a string, concat the next numBTN press with num1 
+// and make that the new num1
+
 const answerDisplayEl = document.getElementById('answer-text');
 const numBTNS = document.querySelectorAll('.num-button');
+const operatorBTNS = document.querySelectorAll('.operator-button');
+const equalBTN = document.getElementById('equal-button');
 
 
-
-
-let num1;
-let num2;
-let operator;
+let num1 = null;
+let num2 = null;
+let operator = null;
+let answer = null;
 
 function add(a, b) {
-  return a + b;
+  return answer = a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  return answer = a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return answer = a * b;
 }
 
 function divide(a, b) {
-  return a / b;
+  return answer =  a / b;
 }
 
-function operate(_num1, _operator, _num2) {
-  switch(_operator) {
-    case '+': add(_num1, _num2);
+function operate() {
+  switch(operator) {
+    case '+': add(num1, num2);
     break;
 
-    case '-': subtract(_num1, _num2);
+    case '-': subtract(num1, num2);
     break;
 
-    case '*': multiply(_num1, _num2);
+    case '*': multiply(num1, num2);
     break;
 
-    case '/': divide(_num1, _num2);
+    case '/': divide(num1, num2);
     break;
   }
+
+  updateDisplay();
 }
 
 numBTNS.forEach(numBTN => {
   numBTN.addEventListener('click', (e) => {
-    num1 = e.target.textContent;
+    operator == null 
+      ? num1 = Number(e.target.textContent) 
+      : num2 = Number(e.target.textContent);
+
     updateDisplay();
   });
 });
 
+operatorBTNS.forEach(operatorBTN => {
+  operatorBTN.addEventListener('click', (e) => {
+    operator = e.target.textContent;
+  });
+});
+
+equalBTN.addEventListener('click', operate);
+
 function updateDisplay() {
-  answerDisplayEl.textContent = num1;
+  operator == null
+    ? answerDisplayEl.textContent = num1
+    : answerDisplayEl.textContent = num2;
+
+  if (answer != null) answerDisplayEl.textContent = answer;
 }
